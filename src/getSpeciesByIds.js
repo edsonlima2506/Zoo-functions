58c0b5b661df6a2b6ec35b02989e219a7f534065
Edson Lima <edsoneduardo526@gmail.com>
2021-09-13 17:15:25 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(ids, ids2) {
+  function verificaId(obj) {
+    if (obj.id === ids || obj.id === ids2) {
+      return obj;
+    }
+  }
+  return species.filter(verificaId);
 }
 
 module.exports = getSpeciesByIds;
