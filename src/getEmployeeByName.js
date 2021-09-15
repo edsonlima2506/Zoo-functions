17629ff4e125b8559b81be1a03c34af72bfaa97c
Edson Lima <edsoneduardo526@gmail.com>
2021-09-14 23:19:40 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  for (let index = 0; index < data.employees.length; index += 1) {
+    const employer = data.employees[index];
+    if (employer.firstName === employeeName || employer.lastName === employeeName) {
+      return data.employees[index];
+    }
+  }
+  if (employeeName === undefined) {
+    return {};
+  }
 }
 
 module.exports = getEmployeeByName;
