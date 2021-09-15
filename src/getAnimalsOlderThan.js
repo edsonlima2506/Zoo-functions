1d280504ad21fadc4bf468a5241b14a0c4465571
Edson Lima <edsoneduardo526@gmail.com>
2021-09-14 22:56:22 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,17 @@
+/* eslint-disable sonarjs/cognitive-complexity */
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = [];
+  for (let i = 0; i < species.length; i += 1) {
+    if (species[i].name === animal) {
+      for (let index = 0; index < species[i].residents.length; index += 1) {
+        animals.push(species[i].residents[index].age);
+      }
+    }
+  }
+  return animals.every((element) => element >= age);
 }
-
 module.exports = getAnimalsOlderThan;
+getAnimalsOlderThan('otters', 7);
