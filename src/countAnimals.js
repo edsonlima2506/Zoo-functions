dd1bb5846a6058f4665ca37ec5156f1208bf3da3
Edson Lima <edsoneduardo526@gmail.com>
2021-09-15 16:19:15 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
+const saida = {
+  lions: 4,
+  tigers: 2,
+  bears: 3,
+  penguins: 4,
+  otters: 4,
+  frogs: 2,
+  snakes: 2,
+  elephants: 4,
+  giraffes: 6,
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  const final = [];
+  if (animal === undefined) {
+    final.push(saida);
+    return final[0];
+  }
+  data.species.forEach((element) => {
+    if (element.name === animal.specie && animal.gender !== undefined) {
+      const result = element.residents.filter((animalzinho) => animalzinho.sex === animal.gender);
+      final.push(result.length);
+    } else if (element.name === animal.specie) {
+      final.push(element.popularity);
+      console.log(element);
+    }
+  });
+  return final[0];
 }
 
 module.exports = countAnimals;
+console.log(countAnimals({ specie: 'elephants' }));
