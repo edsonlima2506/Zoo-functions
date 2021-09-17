3877a56ebd7d0b55bbebb2d2f54c34fcf5255de3
Edson Lima <edsoneduardo526@gmail.com>
2021-09-16 22:04:28 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,37 @@
 const data = require('../data/zoo_data');
 
+const saida = {};
+
 function countEntrants(entrants) {
-  // seu código aqui
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const criancas = [];
+  const adultos = [];
+  const idosos = [];
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      criancas.push(element);
+    } else if (element.age < 50) {
+      adultos.push(element);
+    } else if (element.age >= 50) {
+      idosos.push(element);
+    }
+  });
+  saida.child = criancas.length; saida.adult = adultos.length; saida.senior = idosos.length;
+  return saida;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const saidaFuncao = countEntrants(entrants);
+  let resultado = 0;
+  resultado = saidaFuncao.child * 20.99 + saidaFuncao.adult * 49.99 + saidaFuncao.senior * 24.99;
+  return resultado;
 }
 
-module.exports = { calculateEntry, countEntrants };
+module.exports = { countEntrants, calculateEntry };
+// console.log(countEntrants({}));
+// console.log(calculateEntry(entrada));
